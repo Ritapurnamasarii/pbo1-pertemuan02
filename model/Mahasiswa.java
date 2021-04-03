8d4396b29a54ee1f43c99113dc0e4dc315653908
Ritapurnamasarii <ritapurnamasarii02@gmail.com>
2021-04-03 12:12:31 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ===================");
+  System.out.println("NIDN : 19630412");
+  System.out.println("Nama : Rita Purnamasari");
+  System.out.println("=====================================");
+ }
+}
